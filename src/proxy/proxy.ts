137bde650136919e4eaafcd3f3d12a9ab282eda7
Peshkov Dmitry <dmytrypeshkov@gmail.com>
2022-02-22 17:02:04 +0300

    v2: попытка сделать https

--- src/proxy/proxy.ts
+++ src/proxy/proxy.ts
@@ -1,10 +1,15 @@
 import http from 'http';
 import initConfig from '../config/config';
 import chalk from 'chalk';
+import fs from 'fs';
+import tls from 'tls';
+import {generateCertificate, checkKey} from '../utils/cert';
 
 class ProxyServer {
     private server: http.Server;
     private port: string;
+    private key: string;
+    private certs: Map<string, string>;
 
     /**
    * @constructor
@@ -12,6 +17,8 @@ class ProxyServer {
     constructor(configPath: string) {
         this.server = http.createServer((req, res) => this.handle(req, res));
         this.port = initConfig(configPath, 'proxy').port;
+        this.key = checkKey()!;
+        this.certs = new Map();
     }
 
     private handle(req: http.IncomingMessage, res: http.ServerResponse): void {
@@ -23,6 +30,28 @@ class ProxyServer {
             method: req.method,
             headers,
         };
+
+        if (req.url?.split(':')[0] === 'https') {
+            res.write('HTTP/1.1 200 Connection Established\r\n' +
+                'Proxy-Agent: HTTP_PROXY\r\n' +
+                '\r\n');
+
+            const tlsOptions = {
+                key: this.key,
+                cert: this.getCertificate(req.url.split('.')[0].split('//')[1]),
+                isServer: true,
+            };
+            const answer = http.request(options, (request: http.IncomingMessage) => {
+                if (!request.statusCode) {
+                    throw new Error('Incorrect status code');
+                }
+                res.writeHead(request.statusCode, request.headers);
+                request.pipe(res);
+            });
+            req.pipe(answer);
+            return;
+        }
+
         const answer = http.request(options, (request: http.IncomingMessage) => {
             if (!request.statusCode) {
                 throw new Error('Incorrect status code');
@@ -44,6 +73,30 @@ class ProxyServer {
 
     async stop() {
         this.server.close();
+    }
+
+    private getCertificate(serverName: string) {
+
+        if (this.certs.has(serverName)) {
+            return tls.createSecureContext({
+                key: this.key,
+                cert: this.certs.get(serverName),
+            });
+        }
+
+        const fileName = `../../sh/keys/${serverName}.crt`;
+        if (fs.existsSync(fileName)) {
+            fs.readFile(fileName, 'utf8', (err, data) => {this.certs.set(serverName, data);});
+
+            return tls.createSecureContext({
+                key: this.key,
+                cert: this.certs.get(serverName),
+            });
+        }
+        generateCertificate(serverName);
+        this.getCertificate(serverName); 
+        // рекурсивно вызываем, пока не получится наконец-то создать файл
+        return;
     }
 }
 
---
+++ src/utils/cert.ts
@@ -0,0 +1,39 @@
+import {spawn} from 'child_process';
+import fs from 'fs';
+
+export const checkKey = () => {
+    const fileName = './cert.key';
+    let key: string;
+    if (fs.existsSync(fileName)) {
+        key = fs.readFileSync(fileName, 'utf8');
+        return key;
+    }
+    generateKey();
+    checkKey();
+    return;
+};
+
+export const generateKey = () => {
+    const gen_key = spawn('../../sh/gen_ca.sh');
+
+    gen_key.stdout.once('data', (data) => {
+        fs.writeFile('./cert.key', data, (err) => {
+            if (err) {
+                console.error(err.message);
+            }
+        });
+    });
+};
+
+export const generateCertificate = (serverName: string) => {
+    const gen_cert = spawn('../../sh/gen_cert.sh', 
+                           [serverName, (Math.floor(Math.random() * 1000000000000)).toString()]);
+
+    gen_cert.stdout.once('data', (data) => {
+        fs.writeFile(`./certs/${serverName}.crt`, data, (err) => {
+            if (err) {
+                console.error(err.message);
+            }
+        });
+    });
+};
